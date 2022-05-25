0c392dca729d549211dad9861ad1c07850dcf0bf
Kishor <kishor1124@gmail.com>
2022-05-25 14:18:34 +0530

    node.js examples

--- day3/main.js
+++ day3/main.js
@@ -1,8 +1,6 @@
-console.log('main script begins');
-// anonymous function 
-setTimeout(function() { 
-    console.log('callback executes after 0s')
-} , 0);
-// arrow function as a callback
-setTimeout(() => console.log('callback executes after 1s'), 1000);
-console.log('main script ends');
+// import fs module
+let fs = require("fs");
+let message = 'Simple content\n';
+// writeFileSync creates file it doesn't exist
+fs.writeFileSync('msg.txt', message, {flag: 'a+'});
+console.log('DONE!');
---
+++ day3/mymodule.js
@@ -0,0 +1,5 @@
+module.exports.PI = 3.14;
+module.exports.emp = {id : 100, name : "Raj"}
+module.exports.test = function() {
+    console.log('test() function');
+}
